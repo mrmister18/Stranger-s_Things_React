5eb59552cde3f544fef7d6d5dad5b0bcca354d5b
Isaac <isaachernandez8@gmail.com>
2022-09-01 21:41:37 -0700

    set up signup inputs to update state

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { registerNewUser } from '../api/api_calls';
 
 const Signup = () => {
-    
+    const [newUserName, setNewUserName] = useState("")
+    const [newPassword, setNewPassword] = useState("")
 
     return <div className="signup">
         <h1>Sign up</h1>
         <form>
             <label htmlFor='username'>Username</label>
-            <input id='username' name='username'type="text"></input>
+            <input onChange={(event) => setNewUserName(event.target.value)} id='username' name='username'type="text"></input>
             <label htmlFor='password'>Password</label>
-            <input id='password' name='password' type="password"></input>
-            <button>Login</button>
+            <input onChange={(event) => setNewPassword(event.target.value)} id='password' name='password' type="password"></input>
+            <button>Register</button>
         </form>
     </div>
 }
