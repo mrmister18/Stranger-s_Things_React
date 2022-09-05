5665db2c09b53e6795f5b377d6dc447996f3e230
Isaac <isaachernandez8@gmail.com>
2022-09-04 19:46:02 -0700

    added inputs to add post to update state

--- src/components/Addpost.js
+++ src/components/Addpost.js
@@ -6,12 +6,21 @@ const Addpost = () => {
     const [price, setPrice] = useState("")
     const [location, setLocation] = useState("")
     const [willDeliver, setWillDeliver] = useState(false)
-
+    
     return <div className='addpost'>
         <h1>Add New Post</h1>
-        <form>
-        <label>Title</label>
-        <input></input>
+        <form onSubmit={(event) => event.preventDefault()}>
+        <label htmlFor='title'>Title</label>
+        <input onChange={(event) => setTitle(event.target.value)} id='title'></input><br />
+        <label htmlFor='description'>Description</label>
+        <input onChange={(event) => setDescription(event.target.value)} id='description'></input><br />
+        <label htmlFor='price'>Price</label>
+        <input onChange={(event) => setPrice(event.target.value)} id='price'></input><br />
+        <label htmlFor='location'>Location</label>
+        <input onChange={(event) => setLocation(event.target.value)} id='location'></input><span>(optional)</span><br />
+        <label htmlFor='deliver'>Willing to deliver?</label>
+        <input type="checkbox" onChange={() => willDeliver ? setWillDeliver(false) : setWillDeliver(true)}></input><br />
+        <button>Post</button>
         </form>
     </div>
 }
